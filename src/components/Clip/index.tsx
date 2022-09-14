c203db64a9a9681eec33dcf5b6771560b3881595
刘永志 <50040262+i-coder-keller@users.noreply.github.com>
2022-09-14 22:53:02 +0800

    clip

--- src/components/Clip/index.tsx
+++ src/components/Clip/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react"
+import React, {useCallback, useEffect, useRef, useState} from "react"
 import { useRecoilState } from "recoil"
 import currentFile from "@/store/currentFile"
 import "./index.less"
@@ -16,14 +16,34 @@ const Clip = (props: Props) => {
   const { frameHeight } = props
   const [current, setCurrent] = useRecoilState(currentFile)
   const container = useRef<HTMLDivElement>()
-  const [controlWidth, setControlWidth] = useState<string>("100%")
+  const controlWidth = useCallback(() => {
+    if (container.current) {
+      return `${container.current.clientWidth + container.current.children[0].clientWidth - 12}px`
+    }else {
+      return "100%"
+    }
+  }, [current.frames])
+  const initEvent = () => {
+    const left = document.querySelector("#left")
+    const right = document.querySelector("#right")
+  }
+  const leftEvent = (target: HTMLDivElement) => {
+    target.addEventListener("mousedown", moveEvent)
+  }
+  const moveEvent = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.target.addEventListener("mousemove", (e: any) => {
+      console.log(e)
+    })
+  }
+
   return (
     <div className="clip-container">
-      <div className="workSpace" ref={container}>
-        <div className="workSpace-controls" style={{width: controlWidth}}>
-
+      <div className="workSpace">
+        <div className="workSpace-controls" style={{width: controlWidth()}}>
+          <div className="control-left control" id="left"></div>
+          <div className="control-right control" id="right"></div>
         </div>
-        <div className="workSpace-frames" style={{height: `${frameHeight}px`}}>
+        <div className="workSpace-frames" style={{height: `${frameHeight}px`}} ref={container}>
           {
             current.frames.map(frame => {
               return <img src={frame.image} key={frame.s}/>
